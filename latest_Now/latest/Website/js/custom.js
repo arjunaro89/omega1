8db1962ef71c12bcc95a232dfa348d94d4f39dff
arjunaro89 <arjun.ed02@gmail.com>
2017-10-18 14:14:24 +0530

    Add files via upload

---
+++ latest_Now/latest/Website/js/custom.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function(){
+    $(".dropdown").hover(
+        function() { $('.dropdown-menu', this).stop().fadeIn("fast");
+        },
+        function() { $('.dropdown-menu', this).stop().fadeOut("fast");
+    });
+});
+
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+
+
+
+
+
